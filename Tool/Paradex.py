e5c38e82a96aa393d9139d77cdc733d05ad1c535
alvarogimenezc <alvarogimenezc24@gmail.com>
2025-10-13 22:37:16 +0200

    c
    c

--- Tool/Paradex.py
+++ Tool/Paradex.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 # Parámetros
-moneda = "ETH"
+moneda = "BTC"
 rango_dias = 1
 granularidad = rango_dias * 24 * 60 * 60 * 1000  # ms en X días
 tiempo_actual = int(time.time() * 1000)
@@ -40,25 +40,48 @@ while "next" in data_json and data_json["next"]:
     }
     response = requests.get('https://api.testnet.paradex.trade/v1/funding/data', params=params, headers=headers)
     data_json = response.json()
-    data_funding.extend(data_json.get("results", []))  # añadimos los nuevos registros a la lista resultados
+    data_funding.extend(data_json.get("results", []))  # Añadimos los nuevos registros a la lista resultados
 
-print(data_funding)
+#Ordenamos el resultado, la paginación ppuede ir desordenada
+data_funding = sorted(data_funding, key=lambda x: x["created_at"])
 
-sys.exit()
-
-#Creamos los ejes de la gráfica
+#El json resultante tiene una granularidad de 5 segundos, lo convertimos a APR y granularidad horaria (average)
 fundings=[]
 for posicion in data_funding: 
-    print(posicion)
     fundings.append(float(posicion["funding_rate"])*24*365) #Lo pasamos a APR
 
 fechas=[]
 for posicion in data_funding:
-    fechas.append(datetime.fromtimestamp(posicion["created_at"] / 1000))
+    fechas.append(posicion["created_at"])
+
+i=0
+sumador=0
+cuentalocal=0
+fundings_horarios=[]
+fechas_horarios=[]
+
+#Output buscado: lista de promedio de funding horario en apr y lista de timestamps de ultimo registro del set horario
+while i < len(fundings):
+    try:
+       while datetime.fromtimestamp(fechas[i] / 1000).hour==datetime.fromtimestamp(fechas[i+1] / 1000).hour: 
+         sumador= sumador+fundings[i]
+         cuentalocal+= 1                      
+         i+=1
+    except:
+
+       break
+
+    fundings_horarios.append((sumador/cuentalocal))      #Promedio de valores para el rango horario iterado con el while, en APR
+    fechas_horarios.append(fechas[i]/1000)               #Nos quedamos con el último registro del rango horarioen milisegundos
+    i+=1
+    sumador=0
+    cuentalocal=0
+
+print(fechas_horarios)
 
 #Graficamos
 plt.figure(figsize=(12,6))
-plt.plot(fechas, fundings, marker="o", linestyle="-", color="blue", label="Funding rate")
+plt.plot(fechas_horarios, fundings_horarios, marker="o", linestyle="-", color="blue", label="Funding rate")
 plt.axhline(0, color="red", linestyle="--", linewidth=1)
 plt.title(f"Funding rate {moneda} (últimos {rango_dias} días)")
 plt.xlabel("Fecha")
