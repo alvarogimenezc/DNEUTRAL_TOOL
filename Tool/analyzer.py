3c353037a84fed8a0913c20eb0745d84ca42dc42
alvarogimenezc <alvarogimenezc24@gmail.com>
2025-11-04 23:05:45 +0100

    commit
    c

--- Tool/analyzer.py
+++ Tool/analyzer.py
@@ -11,79 +11,94 @@ def analyzer (moneda, fundings_lighter, fundings_paradex, fundings_hyperliquid):
     sumador=0
 
     #Calculate the mean apr value for the period
-    for funding in fundings_hyperliquid:
-        sumador+=funding
-    hyper_apr=sumador/len(fundings_hyperliquid)
-    sumador=0
+    try:
+        for funding in fundings_hyperliquid:
+            sumador+=funding
+        hyper_apr=sumador/len(fundings_hyperliquid)
+        sumador=0
+    except: 
+        pass
 
-    for funding in fundings_paradex:
-        sumador+=funding
-    paradex_apr=sumador/len(fundings_paradex)
-    sumador=0
+    try:   
+        for funding in fundings_paradex:
+            sumador+=funding
+        paradex_apr=sumador/len(fundings_paradex)
+        sumador=0
+    except: 
+        pass
 
-    for funding in fundings_lighter:
-        sumador+=funding
-    lighter_apr=sumador/len(fundings_lighter)
+    try: 
+        for funding in fundings_lighter:
+            sumador+=funding
+        lighter_apr=sumador/len(fundings_lighter)
+    except: 
+        pass
 
-    #Lets run the simulations paradex-hyperliquid
-    if hyper_apr > 0 and paradex_apr > 0: 
-        if hyper_apr < paradex_apr:
-            delta_para_hype=["Hyperliquid", "Paradex", paradex_apr - hyper_apr]
+    #Lets run the simulations paradex-hyperliquid, after checking both coins are available
+    if fundings_hyperliquid!=[] and fundings_paradex!=[]:
+
+        if hyper_apr > 0 and paradex_apr > 0: 
+            if hyper_apr < paradex_apr:
+                delta_para_hype=["Hyperliquid", "Paradex", paradex_apr - hyper_apr]
+            else:
+                delta_para_hype=["Paradex", "Hyperliquid", hyper_apr - paradex_apr]
+        
+        elif hyper_apr < 0 and paradex_apr < 0: 
+            if hyper_apr < paradex_apr:
+                delta_para_hype=["Hyperliquid", "Paradex", paradex_apr - hyper_apr]
+            else:
+                delta_para_hype=["Paradex", "Hyperliquid", hyper_apr - paradex_apr]
+
         else:
-            delta_para_hype=["Paradex", "Hyperliquid", hyper_apr - paradex_apr]
+            if hyper_apr > 0: 
+               delta_para_hype=["Paradex", "Hyperliquid", hyper_apr - paradex_apr]
+            else: 
+               delta_para_hype=["Hyperliquid", "Paradex", hyper_apr - paradex_apr]
+        delta_para_hype.append(moneda)
+
+
+    #Lets run the simulations paradex-lighter, after checking both coins are available
+    if fundings_lighter!=[] and fundings_paradex!=[]:
+
+        if lighter_apr > 0 and paradex_apr > 0: 
+            if lighter_apr < paradex_apr:
+                delta__para_lig=["Lighter", "Paradex", paradex_apr - lighter_apr]
+            else:
+                delta__para_lig=["Paradex", "Lighter", lighter_apr - paradex_apr]
+        
+        elif lighter_apr < 0 and paradex_apr < 0: 
+            if lighter_apr < paradex_apr:
+                delta__para_lig=["Lighter", "Paradex", paradex_apr - lighter_apr]
+            else:
+                delta__para_lig=["Paradex", "Lighter", lighter_apr - paradex_apr]
+
+        else:
+            if lighter_apr > 0: 
+               delta__para_lig=["Paradex", "Lighter", lighter_apr - paradex_apr]
+            else: 
+               delta__para_lig=["Lighter", "Paradex", lighter_apr - paradex_apr]
+        delta__para_lig.append(moneda)
+ 
+    #Lets run the simulations hyperliquid-lighter, after checking both coins are available
+    if fundings_lighter!=[] and fundings_hyperliquid!=[]:
+   
+        if lighter_apr > 0 and hyper_apr > 0: 
+            if lighter_apr < hyper_apr:
+                delta_lig_hype=["Lighter", "Hyperliquid", hyper_apr - lighter_apr]
+            else:
+                delta_lig_hype=["Hyperliquid", "Lighter", lighter_apr - hyper_apr]
+        
+        elif lighter_apr < 0 and hyper_apr < 0: 
+            if lighter_apr < hyper_apr:
+                delta_lig_hype=["Lighter", "Hyperliquid", hyper_apr - lighter_apr]
+            else:
+                delta_lig_hype=["Hyperliquid", "Lighter", lighter_apr - hyper_apr]
+
+        else:
+            if lighter_apr > 0: 
+               delta_lig_hype=["Hyperliquid", "Lighter", lighter_apr - hyper_apr]
+            else: 
+               delta_lig_hype=["Lighter", "Hyperliquid", lighter_apr - hyper_apr]
+        delta_lig_hype.append(moneda)
     
-    elif hyper_apr < 0 and paradex_apr < 0: 
-        if hyper_apr < paradex_apr:
-            delta_para_hype=["Hyperliquid", "Paradex", paradex_apr - hyper_apr]
-        else:
-            delta_para_hype=["Paradex", "Hyperliquid", hyper_apr - paradex_apr]
-
-    else:
-        if hyper_apr > 0: 
-           delta_para_hype=["Paradex", "Hyperliquid", hyper_apr - paradex_apr]
-        else: 
-           delta_para_hype=["Hyperliquid", "Paradex", hyper_apr - paradex_apr]
-
-    #Lets run the simulations paradex-lighter
-    if lighter_apr > 0 and paradex_apr > 0: 
-        if lighter_apr < paradex_apr:
-            delta__para_lig=["Lighter", "Paradex", paradex_apr - lighter_apr]
-        else:
-            delta__para_lig=["Paradex", "Lighter", lighter_apr - paradex_apr]
-    
-    elif lighter_apr < 0 and paradex_apr < 0: 
-        if lighter_apr < paradex_apr:
-            delta__para_lig=["Lighter", "Paradex", paradex_apr - lighter_apr]
-        else:
-            delta__para_lig=["Paradex", "Lighter", lighter_apr - paradex_apr]
-
-    else:
-        if lighter_apr > 0: 
-           delta__para_lig=["Paradex", "Lighter", lighter_apr - paradex_apr]
-        else: 
-           delta__para_lig=["Lighter", "Paradex", lighter_apr - paradex_apr]
-
-    #Lets run the simulations hyperliquid-lighter
-    if lighter_apr > 0 and hyper_apr > 0: 
-        if lighter_apr < hyper_apr:
-            delta_lig_hype=["Lighter", "Hyperliquid", hyper_apr - lighter_apr]
-        else:
-            delta_lig_hype=["Hyperliquid", "Lighter", lighter_apr - hyper_apr]
-    
-    elif lighter_apr < 0 and hyper_apr < 0: 
-        if lighter_apr < hyper_apr:
-            delta_lig_hype=["Lighter", "Hyperliquid", hyper_apr - lighter_apr]
-        else:
-            delta_lig_hype=["Hyperliquid", "Lighter", lighter_apr - hyper_apr]
-
-    else:
-        if lighter_apr > 0: 
-           delta_lig_hype=["Hyperliquid", "Lighter", lighter_apr - hyper_apr]
-        else: 
-           delta_lig_hype=["Lighter", "Hyperliquid", lighter_apr - hyper_apr]
-
-    delta_para_hype.append(moneda)
-    delta__para_lig.append(moneda)
-    delta_lig_hype.append(moneda)
-
     return(delta_para_hype, delta__para_lig, delta_lig_hype)
--- Tool/main.py
+++ Tool/main.py
@@ -7,6 +7,9 @@ from paradex import paradex
 from plotter import plotter
 from analyzer import analyzer
 
+# Guarda el tiempo de inicio
+tiempo_inicio = time.time()
+
 # Cargamos configuración del yml
 with open("Config.yaml", "r", encoding="utf-8") as file:
     config = yaml.safe_load(file)
@@ -14,13 +17,33 @@ with open("Config.yaml", "r", encoding="utf-8") as file:
 monedas = config["monedas"]
 granularidad = config["granularidad"]
 
-# Iteramos sobre los datos de configuración. Deberemos meter un try except a cada llamada por si la moneda no está disponible
+# Iteramos sobre los datos de configuración. 
 for moneda in monedas: 
-    print(f"\n{moneda}\n")
     
-    fundings_hyperliquid, fechas_hyperliquid= hyperliquid(moneda, granularidad)
-    fundings_lighter, fechas_lighter= lighter(moneda, granularidad)
-    fundings_paradex, fechas_paradex= paradex(moneda, granularidad)
+    try:
+      fundings_hyperliquid, fechas_hyperliquid= hyperliquid(moneda, granularidad)
+    except: 
+      fundings_hyperliquid, fechas_hyperliquid=[], []
+
+    try:
+      fundings_lighter, fechas_lighter= lighter(moneda, granularidad)
+    except:
+        fundings_lighter, fechas_lighter=[], []
+
+    try:
+        fundings_paradex, fechas_paradex= paradex(moneda, granularidad)
+    except:
+        fundings_paradex, fechas_paradex=[], []
 
     #Aquí habria que meter un ID de la estrategia y mostrarlo en formato de lista recursivamente, ordenado de mayor a menor APR
-    print(analyzer(moneda, fundings_lighter, fundings_paradex, fundings_hyperliquid))
+    output1, output2, output3=analyzer(moneda, fundings_lighter, fundings_paradex, fundings_hyperliquid)
+    if output1!=[]:
+        print(output1)
+    if output2!=[]:
+        print(output2)
+    if output3!=[]:
+        print(output3)
+
+# Calcula y muestra el tiempo de ejecución
+tiempo_fin = time.time()
+print(f"El código tardó: {(tiempo_fin - tiempo_inicio)/60} minutos")
